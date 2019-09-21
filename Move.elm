21f6f2b633df976604e6c5ee48598a23f1617ad1
Steve Howell <showell30@yahoo.com>
2019-09-21 14:46:57 -0400

    Start implementing validate_move.
    It just does the first of many tasks--make sure you
    don't land on yourself.

--- Move.elm
+++ Move.elm
@@ -26,6 +26,22 @@ type alias MoveStatus =
     , active_square: Maybe SquareKey
     }
 
+validate_move: Move -> Result String String
+validate_move move =
+    let
+        piece_map = move.piece_map
+        prev = move.prev
+        next = move.next
+
+        source_piece_color = get_piece piece_map prev.zone_color prev.id
+        target_piece_color = get_piece piece_map next.zone_color next.id
+
+    in
+        if source_piece_color == target_piece_color then
+            Err "You cannot land on your own piece"
+        else
+            Ok "success"
+
 perform_move: Move -> MoveStatus
 perform_move move =
     let
@@ -42,19 +58,26 @@ perform_move move =
                 , active_square = Nothing
                 }
             Just piece_color_ ->
-                let
-                    new_config =
-                        { zone_color = next.zone_color
-                        , color = piece_color_
-                        , id = next.id
-                        }
-                    new_map = piece_map
-                        -- TODO: banish piece we're landing on
-                        |> unassign_piece prev
-                        |> assign_piece new_config
-                in
-                    { piece_map = new_map
-                    , status = "moved!"
+                case validate_move move of
+                Err status ->
+                    { piece_map = piece_map
+                    , status = status
                     , active_square = Nothing
                     }
+                Ok _ ->
+                    let
+                        new_config =
+                            { zone_color = next.zone_color
+                            , color = piece_color_
+                            , id = next.id
+                            }
+                        new_map = piece_map
+                            -- TODO: banish piece we're landing on
+                            |> unassign_piece prev
+                            |> assign_piece new_config
+                    in
+                        { piece_map = new_map
+                        , status = "moved!"
+                        , active_square = Nothing
+                        }
 
