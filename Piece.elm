b08e4cc240bcf41e7c92a2eaf9ad2d5694cc3865
Steve Howell <showell30@yahoo.com>
2019-09-24 18:04:02 -0400

    Format Piece.elm.

--- Piece.elm
+++ Piece.elm
@@ -1,50 +1,60 @@
-module Piece exposing
-    ( config_pieces
-    , get_piece
-    , assign_piece
-    , unassign_piece
-    )
+module Piece
+    exposing
+        ( config_pieces
+        , get_piece
+        , assign_piece
+        , unassign_piece
+        )
 
 import Dict
+import Type
+    exposing
+        ( SquareKey
+        , Color
+        , PieceDict
+        )
+import Config
+    exposing
+        ( holding_pen_squares
+        )
 
-import Type exposing
-    ( SquareKey
-    , Color
-    , PieceDict
-    )
-
-import Config exposing
-    ( holding_pen_squares
-    )
 
 type alias PieceConfig =
-    { zone_color: Color
-    , color: Color
-    , id: String
+    { zone_color : Color
+    , color : Color
+    , id : String
     }
 
-get_piece: PieceDict -> Color -> String -> Maybe String
+
+get_piece : PieceDict -> Color -> String -> Maybe String
 get_piece dct color id =
     case Dict.get color dct of
-        Just sub_dict -> Dict.get id sub_dict
-        other -> Nothing
+        Just sub_dict ->
+            Dict.get id sub_dict
 
-config_zone_pieces: String -> PieceDict -> PieceDict
+        other ->
+            Nothing
+
+
+config_zone_pieces : String -> PieceDict -> PieceDict
 config_zone_pieces color_ dct =
     let
         assign id_ =
-            assign_piece {zone_color = color_, color = color_, id = id_}
+            assign_piece { zone_color = color_, color = color_, id = id_ }
     in
         List.foldr assign dct holding_pen_squares
 
-config_pieces: List Color -> PieceDict
+
+config_pieces : List Color -> PieceDict
 config_pieces zone_colors =
     let
-        dct = Dict.empty
+        dct =
+            Dict.empty
     in
         List.foldl config_zone_pieces dct zone_colors
 
-unassign_piece: SquareKey -> PieceDict -> PieceDict
+
+unassign_piece : SquareKey -> PieceDict -> PieceDict
 unassign_piece square_key dct =
     case Dict.get square_key.zone_color dct of
         Nothing ->
@@ -52,27 +62,39 @@ unassign_piece square_key dct =
             -- already checking that a piece is currently
             -- assigned to this square
             dct
+
         Just sub_dict ->
             let
-                new_sub_dict = Dict.remove square_key.id sub_dict
+                new_sub_dict =
+                    Dict.remove square_key.id sub_dict
             in
                 Dict.insert square_key.zone_color new_sub_dict dct
 
-assign_piece: PieceConfig -> PieceDict -> PieceDict
+
+assign_piece : PieceConfig -> PieceDict -> PieceDict
 assign_piece config dct =
     let
-        key = config.zone_color
-        sub_key = config.id
-        val = config.color
+        key =
+            config.zone_color
 
-        maybe_sub_dict = Dict.get key dct
+        sub_key =
+            config.id
 
-        sub_dict = case maybe_sub_dict of
-            Just sd -> sd
-            Nothing -> Dict.empty
+        val =
+            config.color
 
-        new_sub_dict = Dict.insert sub_key val sub_dict
+        maybe_sub_dict =
+            Dict.get key dct
 
+        sub_dict =
+            case maybe_sub_dict of
+                Just sd ->
+                    sd
+
+                Nothing ->
+                    Dict.empty
+
+        new_sub_dict =
+            Dict.insert sub_key val sub_dict
     in
         Dict.insert key new_sub_dict dct
-
