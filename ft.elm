53c04d424c6958ae504ceceb6a0cfc985bfde9cb
Steve Howell <showell30@yahoo.com>
2019-09-24 18:01:18 -0400

    Format ft.elm

--- ft.elm
+++ ft.elm
@@ -2,42 +2,51 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
-import Type exposing
-    ( SquareKind(..)
-    , Square
-    , SquareKey
-    , Color
-    , Model
-    )
-import Config exposing
-    ( orig_zone_colors
-    )
-import Board exposing
-    ( board_view
-    , board_rotate_button
-    , rotate_board
-    )
-import Piece exposing
-    ( get_piece
-    , config_pieces
-    )
-import Move exposing
-    ( perform_move
-    )
-import Player exposing
-    ( config_players
-    , player_view
-    , draw_card_cmd
-    , draw_card
-    , activate_card
-    , finish_card
-    )
-import Square exposing
-    ( square_desc
-    )
-import Msg exposing
-    ( Msg(..)
-    )
+import Type
+    exposing
+        ( SquareKind(..)
+        , Square
+        , SquareKey
+        , Color
+        , Model
+        )
+import Config
+    exposing
+        ( orig_zone_colors
+        )
+import Board
+    exposing
+        ( board_view
+        , board_rotate_button
+        , rotate_board
+        )
+import Piece
+    exposing
+        ( get_piece
+        , config_pieces
+        )
+import Move
+    exposing
+        ( perform_move
+        )
+import Player
+    exposing
+        ( config_players
+        , player_view
+        , draw_card_cmd
+        , draw_card
+        , activate_card
+        , finish_card
+        )
+import Square
+    exposing
+        ( square_desc
+        )
+import Msg
+    exposing
+        ( Msg(..)
+        )
+
 
 -- MODEL / INIT
 
@@ -54,7 +63,8 @@ main =
 init : () -> ( Model, Cmd Msg )
 init flags =
     let
-        zone_colors = orig_zone_colors
+        zone_colors =
+            orig_zone_colors
 
         model =
             { zone_colors = zone_colors
@@ -63,14 +73,17 @@ init flags =
             , active_square = Nothing
             , players = config_players zone_colors
             }
-
     in
         ( model, Cmd.none )
 
+
 active_color : Model -> Color
 active_color model =
+    -- appease compiler with Maybe
     List.head model.zone_colors
-        |> Maybe.withDefault "bogus" -- appease compiler
+        |> Maybe.withDefault "bogus"
+
+
 
 -- UPDATE
 
@@ -80,80 +93,98 @@ update msg model =
     case msg of
         ClickSquare clicked_square ->
             let
-                model_ = handle_square_click model clicked_square
+                model_ =
+                    handle_square_click model clicked_square
             in
-                (model_, Cmd.none)
+                ( model_, Cmd.none )
+
         DrawCard player_color ->
             ( model
             , draw_card_cmd model.players player_color
             )
+
         DrawCardResult player_color idx ->
             let
                 model_ =
                     { model
-                    | players = draw_card model.players player_color idx
+                        | players = draw_card model.players player_color idx
                     }
             in
-                (model_, Cmd.none)
+                ( model_, Cmd.none )
+
         ActivateCard player_color idx ->
             let
                 model_ =
                     { model
-                    | players = activate_card model.players player_color idx
+                        | players = activate_card model.players player_color idx
                     }
             in
-                (model_, Cmd.none)
+                ( model_, Cmd.none )
+
         FinishCard player_color ->
             let
                 model_ =
                     { model
-                    | players = finish_card model.players player_color
+                        | players = finish_card model.players player_color
                     }
             in
-                (model_, Cmd.none)
+                ( model_, Cmd.none )
+
         RotateBoard ->
             let
                 model_ =
                     { model
-                    | zone_colors = rotate_board model.zone_colors
+                        | zone_colors = rotate_board model.zone_colors
                     }
             in
-                (model_, Cmd.none)
+                ( model_, Cmd.none )
 
-handle_square_click: Model -> SquareKey -> Model
+
+handle_square_click : Model -> SquareKey -> Model
 handle_square_click model clicked_square =
     case model.active_square of
         Nothing ->
             let
-                piece_color = get_piece model.piece_map clicked_square.zone_color clicked_square.id
-                active_square = case piece_color of
-                    Nothing ->
-                        Nothing
-                    Just piece_color_ ->
-                        if piece_color_ == (active_color model) then
-                            Just clicked_square
-                        else
+                piece_color =
+                    get_piece model.piece_map clicked_square.zone_color clicked_square.id
+
+                active_square =
+                    case piece_color of
+                        Nothing ->
                             Nothing
-                desc = square_desc model.piece_map clicked_square piece_color
-                status = case active_square of
-                    Just _ ->
-                        desc ++ " (CLICK square to move piece)"
-                    Nothing ->
-                        desc
+
+                        Just piece_color_ ->
+                            if piece_color_ == (active_color model) then
+                                Just clicked_square
+                            else
+                                Nothing
+
+                desc =
+                    square_desc model.piece_map clicked_square piece_color
+
+                status =
+                    case active_square of
+                        Just _ ->
+                            desc ++ " (CLICK square to move piece)"
+
+                        Nothing ->
+                            desc
             in
                 { model
-                | status = status
-                , active_square = active_square
+                    | status = status
+                    , active_square = active_square
                 }
+
         Just prev ->
             let
                 move =
                     { prev = prev
                     , next = clicked_square
                     }
-
             in
                 perform_move model move
+
+
 
 -- SUBSCRIPTIONS
 
@@ -163,21 +194,25 @@ subscriptions model =
     Sub.none
 
 
+
 -- VIEW
 
 
 view : Model -> Browser.Document Msg
 view model =
     let
-        heading = div
-            []
-            [ Html.text model.status ]
+        heading =
+            div
+                []
+                [ Html.text model.status ]
 
-        board = div
-            []
-            [ board_view model.piece_map model.zone_colors model.active_square]
+        board =
+            div
+                []
+                [ board_view model.piece_map model.zone_colors model.active_square ]
 
-        cards = player_view model.players (active_color model)
+        cards =
+            player_view model.players (active_color model)
 
         body =
             [ heading
