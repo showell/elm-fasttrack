3a97fbf3ad4c4fc955800de1ab5a9007d324d323
Steve Howell <showell30@yahoo.com>
2019-09-24 17:59:06 -0400

    Format Deck.elm.

--- Deck.elm
+++ Deck.elm
@@ -1,6 +1,7 @@
-module Deck exposing ( full_deck )
+module Deck exposing (full_deck)
 
-import Type exposing ( Card )
+import Type exposing (Card)
+
 
 -- We ignore suits in FastTrack, since they don't affect game play.
 -- Also, we never truly shuffle the deck; instead, we remove cards
